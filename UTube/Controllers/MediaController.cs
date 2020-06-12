e2f1a8fb95a87374929f56b1c2e393c7f958bcdf
KingofZoO <38123328+KingofZoO@users.noreply.github.com>
2020-06-12 20:38:26 +0700

    Add project files.

---
+++ UTube/Controllers/MediaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UTube.Controllers {
+    [Route("api")]
+    public class MediaController : Controller {
+        private const string mediaFolder = "mediaFolder";
+
+        private readonly string mediaPath;
+        private readonly DirectoryInfo mediaDirectory;
+
+        public MediaController() {
+            mediaPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, mediaFolder);
+            mediaDirectory = new DirectoryInfo(mediaPath);
+
+            if (!mediaDirectory.Exists)
+                Directory.CreateDirectory(mediaPath);
+        }
+
+        [HttpGet]
+        [Route("medialist")]
+        public IEnumerable<string> GetMediaList() => mediaDirectory.EnumerateFiles().Select(file => file.Name);
+
+        [HttpGet]
+        [Route("mediafile")]
+        public PhysicalFileResult GetMediaFile(string name) => new PhysicalFileResult(Path.Combine(mediaPath, name), "application/octet-stream");
+    }
+}
---
+++ UTube/MediaService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace UTube {
+    public class MediaService {
+        private readonly string mediaAPIAddress;
+        private readonly string medialistCommand;
+
+        private readonly HttpClient httpClient;
+
+        public MediaService() {
+            mediaAPIAddress = "https://localhost:44307/api/";
+            medialistCommand = "medialist";
+
+            httpClient = new HttpClient();
+        }
+
+        public string[] GetMediaList() => httpClient.GetFromJsonAsync<string[]>(APICommand(medialistCommand)).Result;
+
+        public string GetUrlByName(string name) => $"{mediaAPIAddress}mediafile?name={name}";
+
+        private string APICommand(string command) => mediaAPIAddress + command;
+    }
+}
